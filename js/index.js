dcb06bf9c9724908332191a2ddb7d00ac56c355b
SEONG_WON <kimsungwon57@naver.com>
2022-12-29 18:19:36 +0900

    1229 작업물

--- js/index.js
+++ js/index.js
@@ -6,3 +6,35 @@ toggleBtn.addEventListener('click', () => {
     menuEl.classList.toggle('active');
     utilityEl.classList.toggle('active');
 });
+
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    loopAdditionalSlides : 1,
+
+    touchRatio: 0,//드래그 금지
+    
+    autoplay: {
+    delay: 3000,
+    },
+
+    pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable : true,
+    },
+    
+    // 슬라이드 사이 여백
+    spaceBetween: 100,    
+    // slidesPerView : 'auto', //
+
+    navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+    },
+
+});
+
+AOS.init(); // 자바스크립트로 init()을 해야 동작한다.
